6dfb52c07c0a88880a0ac24cbed519f7fbafade8
Antu Saha <antusaha970@gmail.com>
2024-06-29 19:20:54 +0600

    added index template

--- auth_project/urls.py
+++ auth_project/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import homepage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include("accounts.urls"))
+    path("api/", include("accounts.urls")),
+    path("", homepage, name="homepage"),
 ]
 
 
